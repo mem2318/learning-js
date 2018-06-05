847bbe204f93845fb30e32ee311472de6604f9db
Megan Massey <megmasse@wkmus8056060.global.publicisgroupe.net>
2018-06-05 15:07:49 -0400

    playing around with node

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-var logger = require('./logger');
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+//Register a listener 
+emitter.on('messageLogged', (arg)=>{
+	console.log('Listener called', arg);
+});
+
+const Logger = require('./logger');
+const logger = new Logger();
 
 logger.log('message');
